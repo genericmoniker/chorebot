dbd6142f1f2804c0ced2c7dce48869dfcc5d4086
Eric Smith <eric@esmithy.net>
2015-07-11 15:47:26 -0600

    Shuffle daily chores among all the "to do" lists on the board.

--- chorebot/chores.py
+++ chorebot/chores.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+import random
 from chorebot.config import create_client
 from chorebot.log import get_logger
 
@@ -8,27 +9,40 @@ logger = get_logger(__name__)
 def daily_update():
     client = create_client()
     chores_board = get_chores_board(client)
+    to_do_lists = get_to_do_lists(chores_board)
+    daily_chores = []
     cards = chores_board.get_cards()
-    reset_personal_daily_chores(client, cards)
-
-
-def reset_personal_daily_chores(client, cards):
     for card in cards:
+        card.fetch(False)
         if has_label(card, 'Personal Daily'):
-            card.set_due(datetime.utcnow())
-            move_to_correct_list(client, card)
+            update_personal_daily_chore(card)
+        elif has_label(card, 'Daily'):
+            set_due_today(card)
+            daily_chores.append(card)
+    deal(daily_chores, to_do_lists)
 
 
-def move_to_correct_list(client, card):
-    list_ = find_todo_list_for_chore(client, card)
-    if list_ and card.list_id != list_.id:
-        card.change_list(list_.id)
-        logger.info(
-            'Card "{}" moved to list "{}".'.format(
-                card.name,
-                list_.name
-            )
-        )
+def update_personal_daily_chore(card):
+    set_due_today(card)
+    todo_list = find_todo_list_by_member(card)
+    if todo_list and card.list_id != todo_list.id:
+        card.change_list(todo_list.id)
+
+
+def deal(cards, lists):
+    """Shuffle and deal cards to lists."""
+    list_i = 0
+    random.shuffle(cards)
+    while cards:
+        cards.pop().change_list(lists[list_i % len(lists)].id)
+        list_i += 1
+
+
+def set_due_today(card):
+    # The dates in the library and API are unclear; hack by adding a day, which
+    # seems to produce the desired results.
+    card.set_due(datetime.now() + timedelta(days=1))
+
 
 def has_label(card, label_name):
     for label in card.labels:
@@ -45,12 +59,23 @@ def get_chores_board(client):
     raise Exception('Chores board not found.')
 
 
-def find_todo_list_for_chore(client, card):
-    """Find the todo list for a chore card."""
+def get_to_do_lists(board):
+    """Get all the 'to do' lists from a board."""
+    todo = []
+    lists = board.get_lists(None)
+    for list_ in lists:
+        name = list_.name.lower()
+        if 'todo' in name or 'to do' in name:
+            todo.append(list_)
+    return todo
+
+
+def find_todo_list_by_member(card):
+    """Find the todo list for a chore card according to the assigned member."""
     # Figure out who the chore is assigned to and find that to do list.
     if not card.member_ids:
         return None
-    member = client.get_member(card.member_ids[0])
+    member = card.client.get_member(card.member_ids[0])
     first_name = member.full_name.split(' ')[0]
     lists = card.board.get_lists(None)
     for list_ in lists:
--- chorebot/main.py
+++ chorebot/main.py
@@ -1,3 +1,4 @@
+import sys
 from apscheduler.schedulers.blocking import BlockingScheduler
 from chorebot.chores import daily_update
 from chorebot.log import setup_logging
@@ -6,12 +7,17 @@ from chorebot.log import setup_logging
 def main():
     setup_logging()
     scheduler = BlockingScheduler()
+    run_now = len(sys.argv) > 1
     daily_trigger = dict(
         trigger='cron',
         day_of_week='mon-sat',
         hour='2'
     )
-    scheduler.add_job(daily_update, **daily_trigger)
+    if run_now:
+        scheduler.add_job(daily_update)
+    else:
+        scheduler.add_job(daily_update, **daily_trigger)
+
     scheduler.start()
 
 if __name__ == '__main__':
