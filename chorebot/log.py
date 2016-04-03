7c151954f3ee74d55ff75392822d59756729d177
Eric Smith <eric@esmithy.net>
2016-04-02 20:24:00 -0600

    Minor PEP8.

--- chorebot/log.py
+++ chorebot/log.py
@@ -1,6 +1,7 @@
 import logging
 from sys import stdout
 
+
 def setup_logging():
     fmt = '%(asctime)s %(levelname)1.1s %(message)s'
     logging.basicConfig(format=fmt, level=logging.INFO, stream=stdout)
